6981a3d7b4e89f9799ab75599774904b18c0297d
mitsimi <noah.fuereder@gmail.com>
2025-10-29 21:50:39 +0100

    feat(repositories): Add repository details view and enhance list item
    Implement repository details view with metadata and improve
    repository list item rendering. Add toggle for showing details
    with keyboard shortcut and dynamic accessories based on sort
    filter. Enhance visual representation with language colors and
    additional repository information.

--- src/my-repositories.tsx
+++ src/my-repositories.tsx
@@ -1,26 +1,87 @@
-import { List } from "@raycast/api";
-import { RepositoryMenu, RepositoryDropdown } from "./components/repositories";
+import { Action, Color, Icon, List } from "@raycast/api";
+import { RepositoryDropdown, RepositoryActions } from "./components/repositories";
 import { useCachedState } from "@raycast/utils";
 import { RepositorySortOption, RepositorySortTypes } from "./types/sorts/repository-search";
 import { useUserRepositories } from "./hooks/useUserRepositories";
+import dayjs from "dayjs";
+import { Repository } from "./types/repository";
+
+import GitHubColors from "./utils/colors";
+import RepositoryDetails from "./components/repositories/repository-details";
+import { useState } from "react";
 
 export default function Command() {
   const [sort, setSort] = useCachedState<RepositorySortOption>(RepositorySortOption.RecentlyUpdated);
+  const [showDetails, setShowDetails] = useState<boolean>(false);
 
   const { items, isLoading } = useUserRepositories(sort);
 
   return (
     <List
       isLoading={isLoading}
+      isShowingDetail={showDetails}
       searchBarAccessory={
         <RepositoryDropdown
           repoFilter={RepositorySortTypes}
-          onFilterChange={(v: string) => setSort(v as RepositorySortOption)}
+          onFilterChange={(newValue: string) => setSort(newValue as RepositorySortOption)}
         />
       }
       throttle
     >
-      <RepositoryMenu items={items} currentFilter={sort} />
+      {items.map((item) => (
+        <List.Item
+          key={item.id}
+          icon={item.avatar_url ? { source: item.avatar_url } : { source: "repo.svg", tintColor: Color.PrimaryText }}
+          title={item.full_name}
+          subtitle={item.description}
+          detail={<RepositoryDetails repo={item} />}
+          actions={
+            <RepositoryActions item={item}>
+              <Action
+                title={showDetails ? "Hide Details" : "Show Details"}
+                icon={showDetails ? Icon.EyeDisabled : Icon.Eye}
+                shortcut={{
+                  macOS: { modifiers: ["cmd", "shift"], key: "x" },
+                  windows: { modifiers: ["ctrl", "shift"], key: "x" },
+                }}
+                onAction={() => setShowDetails(!showDetails)}
+              />
+            </RepositoryActions>
+          }
+          accessories={
+            showDetails
+              ? []
+              : ([
+                  ...(item.language
+                    ? [{ tag: { value: item.language, color: GitHubColors.get(item.language, true)?.color } }]
+                    : []),
+                  getAccessoryByFilter(item, sort),
+                ] as List.Item.Accessory[])
+          }
+        />
+      ))}
     </List>
   );
+}
+
+function getAccessoryByFilter(item: Repository, filter?: string): List.Item.Accessory {
+  switch (filter) {
+    case "most stars":
+    case "fewest stars":
+      return { icon: Icon.Star, text: { value: `${item.stars_count}`, color: Color.SecondaryText } };
+    case "newest":
+    case "oldest":
+      return {
+        icon: Icon.Calendar,
+        date: dayjs(item.created_at).toDate(),
+      };
+    case "recently":
+    case "least recently":
+      return {
+        icon: Icon.Calendar,
+        date: dayjs(item.updated_at).toDate(),
+      };
+  }
+
+  return {};
 }
